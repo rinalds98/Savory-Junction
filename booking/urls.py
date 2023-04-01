51d7c60ec29d0077a174e42099daeb69eb077efd
rinalds98 <rinalds98@gmail.com>
2023-04-01 12:54:27 +0000

    wire up urls to html templates to navigate website frontend

--- booking/urls.py
+++ booking/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.BookingList.as_view(), name='home')
+    path('', views.IndexView.as_view(), name='home'),
+    path('reservations.html', views.BookingList.as_view(), name='reservations'),
+    path('menu.html', views.MenuView.as_view(), name='menu'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic import TemplateView
 from .models import Booking
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class MenuView(TemplateView):
+    template_name = 'menu.html'
 
 
 class BookingList(CreateView):
     model = Booking
     fields = ['day', 'time']
-    template_name = 'index.html'
+    template_name = 'reservations.html'
+
