370b40fdb3af6f02d4f722d5d319d959b9418ae7
rinalds98 <rinalds98@gmail.com>
2023-04-10 13:29:13 +0000

    modify form.py to use radio boxes and adjust css to have stars as radio boxes

--- booking/forms.py
+++ booking/forms.py
@@ -1,8 +1,18 @@
 from .models import Review
 from django import forms
 
+STAR_CHOICES = (
+    (1, ''),
+    (2, ''),
+    (3, ''),
+    (4, ''),
+    (5, ''),
+)
+
 
 class ReviewForm(forms.ModelForm):
+    rating = forms.ChoiceField(choices=STAR_CHOICES, widget=forms.RadioSelect(attrs={'class': 'icon'}))
+
     class Meta:
         model = Review
         fields = ["comment", "rating"]
