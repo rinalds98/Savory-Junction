5b7c62a47e5a60356940b3873c00e713f92f87c6
rinalds98 <rinalds98@gmail.com>
2023-04-11 20:40:12 +0000

    Fix star rating system to work properly -adjust css and reverse tuple in form

--- booking/forms.py
+++ booking/forms.py
@@ -2,11 +2,11 @@ from .models import Review
 from django import forms
 
 STAR_CHOICES = (
-    (1, ""),
-    (2, ""),
-    (3, ""),
-    (4, ""),
     (5, ""),
+    (4, ""),
+    (3, ""),
+    (2, ""),
+    (1, ""),
 )
 
 
