82183806643d11bfe8ade76c561083a024cb3188
rinalds98 <rinalds98@gmail.com>
2023-04-11 21:26:53 +0000

    Modify bootstrap css adjusting row and columns  for index.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,10 @@
+// This function provides a custom datepicker for reservations.
 $( function() {
     $( "#datepicker" ).datepicker();
   } );
 
+// This function sets a timer for alerts so they don't
+// stay on the screen forever.
 setTimeout(function(){
   let messages = document.getElementById("msg");
   let alert = new bootstrap.Alert(messages);
