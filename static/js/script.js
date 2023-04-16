e699635476a81e1cc9e60d4b0b9c8454049c310e
rinalds98 <rinalds98@gmail.com>
2023-04-16 13:32:32 +0000

    Deployment commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,13 @@
 // This function provides a custom datepicker for reservations.
 // This function was taken from https://jqueryui.com/datepicker/
 $( function() {
-  $( "#datepicker" ).datepicker();
-} );
+    $( "#datepicker" ).datepicker();
+  } );
 
 // This function sets a timer for alerts so they don't
 // stay on the screen forever.
 setTimeout(function(){
-let messages = document.getElementById("msg");
-let alert = new alert(messages);
-alert.close();
+  let messages = document.getElementById("msg");
+  let alert = new bootstrap.alert(messages);
+  alert.close();
 },3000);
